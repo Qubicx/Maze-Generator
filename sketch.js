04f8e5eee69f0011fd4948cfcd538c413a9216ab
AMH <waddley8@gmail.com>
2020-08-01 19:22:43 -0400

    Added Cell class

--- sketch.js
+++ sketch.js
@@ -1,7 +1,45 @@
+let cellSize = 10;
+
 function setup() {
-  // put setup code here
+  createCanvas(400, 400);
+
 }
 
 function draw() {
-  // put drawing code here
+  background(220);
+  let cells = []
+  for (let i = 0; i < width / cellSize; i++) {
+    for (let j = 0; j < height / cellSize; j++) {
+      cells.push(new Cell(i * cellSize,j * cellSize, [random([true,false]),random([true,false]),random([true,false]),random([true,false])]));
+      cells[cells.length-1].show();
+    }
+  }
+  noLoop();
+}
+
+class Cell {
+  constructor(x_, y_, walls_ = [true, true, true, true]) {
+    this.x = x_;
+    this.y = y_;
+    this.walls = walls_;
+  }
+  show() {
+    fill(255);
+    noStroke();
+    rect(this.x, this.y, cellSize, cellSize);
+    stroke(0);
+    strokeWeight(1);
+    if (this.walls[0]) {
+      line(this.x, this.y, this.x + cellSize, this.y);
+    }
+    if (this.walls[1]) {
+      line(this.x + cellSize, this.y, this.x + cellSize, this.y + cellSize);
+    }
+    if (this.walls[2]) {
+      line(this.x, this.y + cellSize, this.x + cellSize, this.y + cellSize);
+    }
+    if (this.walls[3]) {
+      line(this.x, this.y, this.x, this.y + cellSize);
+    }
+  }
 }
