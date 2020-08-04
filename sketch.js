d38ddb21b9c4e5a88493cc7119bb30577d8a1355
AMH <waddley8@gmail.com>
2020-08-03 21:34:33 -0400

    Hurricane commit

--- sketch.js
+++ sketch.js
@@ -1,34 +1,69 @@
 let cellSize = 10;
+let cells = [];
+let mX = 10;
+let mY = 10;
 
 function setup() {
   createCanvas(400, 400);
-
+  background(220);
+  frameRate(1)
 }
 
 function draw() {
-  background(220);
-  let cells = []
-  for (let i = 0; i < width / cellSize; i++) {
-    for (let j = 0; j < height / cellSize; j++) {
-      cells.push(new Cell(i * cellSize,j * cellSize, [random([true,false]),random([true,false]),random([true,false]),random([true,false])]));
-      cells[cells.length-1].show();
-    }
+  cells.push(new Cell(mX * cellSize, mY * cellSize, shuffle([0, 1, 2, 3])));
+  direction = cells[cells.length - 1].dirOrder.shift();
+
+  showAll();
+  mX += cardinalTranslate(direction)[0];
+  mY += cardinalTranslate(direction)[1];
+}
+
+function showAll() {
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].show();
   }
-  noLoop();
+}
+
+function cardinalTranslate(direction) {
+  switch (direction) {
+    case 0: //north
+      return [0, -1];
+      break;
+    case 1: //east
+      return [1, 0];
+      break;
+    case 2: //south
+      return [0, 1];
+      break;
+    case 3: //west
+      return [-1, 0];
+      break;
+    default:
+      return [0, 0];
+      break;
+  }
 }
 
 class Cell {
-  constructor(x_, y_, walls_ = [true, true, true, true]) {
+  constructor(x_, y_, dirOrder_ = [0, 1, 2, 3], walls_ = [false, false, true, true]) {
     this.x = x_;
     this.y = y_;
     this.walls = walls_;
+    this.dirOrder = dirOrder_;
+    this.firstDraw = true;
   }
-  show() {
-    fill(255);
+  show() { //draw the cell
     noStroke();
+    if (this.firstDraw) {
+      fill(128, 128, 255);
+      this.firstDraw = false;
+    } else {
+      fill(255);
+    }
     rect(this.x, this.y, cellSize, cellSize);
     stroke(0);
     strokeWeight(1);
+    strokeCap(PROJECT);
     if (this.walls[0]) {
       line(this.x, this.y, this.x + cellSize, this.y);
     }
