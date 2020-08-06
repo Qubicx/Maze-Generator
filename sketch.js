d1df53ae885d38d9bc703397cad0042f51cc71f3
AMH <waddley8@gmail.com>
2020-08-06 19:44:10 -0400

    Distinguished between canvas dimentions and maze dimentions

--- sketch.js
+++ sketch.js
@@ -10,8 +10,8 @@ function setup() {
 }
 
 function draw() {
-  cells.push(new Cell(mX * cellSize, mY * cellSize));
-  direction = cells[cells.length - 1].dirOrder.shift();
+  cells.push(new Cell(mX, mY));
+  direction = cells[cells.length - 1].directions.shift();
 
   showAll();
   mX += cardinalTranslate(direction)[0];
@@ -54,14 +54,16 @@ function cardinalTranslate(direction) {
 }
 
 class Cell {
-  constructor(x_, y_,  walls_ = [true, true, true, true], dirOrder_ = [0, 1, 2, 3]) {
+  constructor(x_, y_,  walls_ = [true, true, true, true]) {
     this.x = x_;
     this.y = y_;
     this.walls = walls_;
-    this.dirOrder = shuffle([0, 1, 2, 3]);
+    this.directions = shuffle([0, 1, 2, 3]);
     this.firstDraw = true;
   }
   show() { //draw the cell
+    this.cX = this.x * cellSize;
+    this.cY = this.y * cellSize;
     noStroke();
     if (this.firstDraw) {
       fill(128, 128, 255);
@@ -69,21 +71,21 @@ class Cell {
     } else {
       fill(255);
     }
-    rect(this.x, this.y, cellSize, cellSize);
+    rect(this.cX, this.cY, cellSize, cellSize);
     stroke(0);
     strokeWeight(1);
     strokeCap(PROJECT);
     if (this.walls[0]) {
-      line(this.x, this.y, this.x + cellSize, this.y);
+      line(this.cX, this.cY, this.cX + cellSize, this.cY);
     }
     if (this.walls[1]) {
-      line(this.x + cellSize, this.y, this.x + cellSize, this.y + cellSize);
+      line(this.cX + cellSize, this.cY, this.cX + cellSize, this.cY + cellSize);
     }
     if (this.walls[2]) {
-      line(this.x, this.y + cellSize, this.x + cellSize, this.y + cellSize);
+      line(this.cX, this.cY + cellSize, this.cX + cellSize, this.cY + cellSize);
     }
     if (this.walls[3]) {
-      line(this.x, this.y, this.x, this.y + cellSize);
+      line(this.cX, this.cY, this.cX, this.cY + cellSize);
     }
   }
 }
